c3d298d38d6bc7a0d19fc0037d5a7c5f7e0a9ffd
nehajyoti <nehajyoti04@gmail.com>
2017-12-28 17:17:25 +0530

    Left and RIght indices added.

--- src/Plugin/views/style/viewsMatrix.php
+++ src/Plugin/views/style/viewsMatrix.php
@@ -34,13 +34,13 @@ class viewsMatrix extends StylePluginBase {
   protected function defineOptions() {
     $options = parent::defineOptions();
     $options['path'] = array('default' => 'views_matrix');
-//    $options['columns'] = ['default' => '4'];
-//    $options['automatic_width'] = ['default' => TRUE];
-//    $options['alignment'] = ['default' => 'horizontal'];
-//    $options['col_class_custom'] = ['default' => ''];
-//    $options['col_class_default'] = ['default' => TRUE];
-//    $options['row_class_custom'] = ['default' => ''];
-//    $options['row_class_default'] = ['default' => TRUE];
+    $options['columns'] = ['default' => '4'];
+    $options['automatic_width'] = ['default' => TRUE];
+    $options['alignment'] = ['default' => 'horizontal'];
+    $options['col_class_custom'] = ['default' => ''];
+    $options['col_class_default'] = ['default' => TRUE];
+    $options['row_class_custom'] = ['default' => ''];
+    $options['row_class_default'] = ['default' => TRUE];
     return $options;
   }
 
@@ -162,6 +162,66 @@ class viewsMatrix extends StylePluginBase {
       '#description' => t('You may use token substitutions from the rewriting section in this class.'),
       '#default_value' => $this->options['yconfig']['class']
     );
+
+    $form['sortable_headers'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Make column headers sort links'),
+      '#description' => t('Emulates the same behavior for the column headers as in an ordinary table. Only makes sense if there will be exactly one result and field value per cell.'),
+      '#default_value' => !empty($this->options['sortable_headers']),
+    );
+
+    $form['header_init_sort'] = array(
+      '#type' => 'select',
+      '#title' => t('Inital sorting direction'),
+      '#description' => t('Direction of the initial sort when a header is clicked for the first time.'),
+      '#options' => array('asc' => t('Ascending'), 'desc' => t('Descending')),
+      '#default_value' => isset($this->options['header_init_sort']) ? $this->options['header_init_sort'] : 'asc',
+      '#dependency' => array(
+        'edit-style-options-sortable-headers' => array(1),
+      ),
+    );
+
+    $form['hide_empty_columns'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Hide empty columns'),
+      '#description' => t('A column will be empty when "hide with empty" is checked for fields that are displayed and there are no results with data.'),
+      '#default_value' => $this->options['hide_empty_columns'],
+    );
+
+    $form['hide_empty_rows'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Hide empty rows'),
+      '#description' => t('A row will be empty when "hide with empty" is checked for fields that are displayed and there are no results with data.'),
+      '#default_value' => $this->options['hide_empty_rows'],
+    );
+
+    $form['one_result_per_cell'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Limit to one result per table cell'),
+      '#description' => t('If enabled, only the first Views result row encountered will be added to each table cell. Subsequent results with the same values for the column and row header fields will be ignored.'),
+      '#default_value' => $this->options['one_result_per_cell'],
+    );
+
+    $form['field_positions'] = array(
+      '#type' => 'details',
+      '#title' => t('Field position'),
+      '#description' => t('For each field, choose where to display it.'),
+      '#open' => TRUE,
+      '#tree' => TRUE,
+    );
+
+    foreach ($field_options as $field_name => $field_label) {
+      $form['field_positions'][$field_name] = array(
+        '#type' => 'select',
+        '#title' => $field_label,
+        '#default_value' => !empty($this->options['field_positions'][$field_name]) ? $this->options['field_positions'][$field_name] : '',
+        '#options' => array(
+          '' => t('Inside the matrix'),
+          'left' => t('On the left of the matrix'),
+          'right' => t('On the right of the matrix'),
+        ),
+      );
+    }
 
   }
 
